1681a6c1e5925d529a6eecef29cb428fad1cf6c2
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-11-21 18:30:13 +0200

    create validation username and password

---
+++ src/utils/validation.py
@@ -0,0 +1,29 @@
+import re
+
+from typing import Optional
+
+from . import massage
+
+
+def password_valid(password: str) -> Optional[str]:
+    """Валидация пароля
+    -------------------
+    * прописные буквы [A-Z]
+    * строчные буквы [a-z]
+    * числа [0-9]
+    * любой из специальных символов [@#$%^&+=]
+    * количество символов [6-20]
+    """
+    if not re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{6,20}', password):
+        return massage.ERROR_PASSWORD_VALID
+
+
+def username_valid(username: str) -> Optional[str]:
+    """Валидация имя_пользователя
+    -----------------------------
+    * прописные буквы [A-Z]
+    * строчные буквы [a-z]
+    * количество символов [4-20]
+    """
+    if not re.fullmatch(r'[A-Za-z]{4,20}', username):
+        return massage.ERROR_USERNAME_VALID
