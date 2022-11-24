e386081ed7d58bff70f2a1e55270efbe5d052709
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-11-24 14:43:02 +0200

    create function form_valid

--- src/utils/validation.py
+++ src/utils/validation.py
@@ -1,6 +1,6 @@
 import re
 
-from typing import Optional
+from typing import Optional, List, Union
 
 from . import massage
 
@@ -36,3 +36,30 @@ def username_valid(username: str) -> Optional[str]:
     """
     if not re.fullmatch(r'[A-Za-z]{4,20}', username):
         return massage.ERROR_USERNAME_VALID
+
+
+def form_valid(
+    username: str,
+    password: str,
+    confirm_password: str = None
+) -> Union[List[str], List]:
+    """Валидация формы"""
+    error_message_list = []
+
+    username_error = username_valid(username)
+    password_error = password_valid(password)
+
+    if username_error:
+        error_message_list.append(username_error)
+
+    if password_error:
+        error_message_list.append(password_error)
+
+    if isinstance(confirm_password, str):
+        confirm_password_error = password_confirm_valid(
+            password, confirm_password
+        )
+        if confirm_password_error:
+            error_message_list.append(confirm_password_error)
+
+    return error_message_list
