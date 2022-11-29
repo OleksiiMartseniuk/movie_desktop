3ee74c0ae604902daf8ca9c62b6006db06f46af9
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-11-29 15:34:16 +0200

    create auth module

---
+++ src/services/auth/security.py
@@ -0,0 +1,38 @@
+from typing import Optional, Union
+
+from passlib.context import CryptContext
+
+from src.services.db import user as user_db
+from src.utils import massage
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Сопоставления хеш-пароля с паролем"""
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password) -> str:
+    """Получить хеш-пароля"""
+    return pwd_context.hash(password)
+
+
+def registration(username: str, password: str) -> Optional[str]:
+    """Регистрация пользователя"""
+    if user_db.get(username=username):
+        return massage.ERROR_USER_EXIST
+    return user_db.create(username, get_password_hash(password))
+
+
+def login(username: str, password: str) -> Union[user_db.User, str]:
+    """Авторизация пользователя"""
+    user = user_db.get(username=username)
+    if user:
+        if verify_password(password, user.password_hash):
+            return user
+        else:
+            return massage.ERROR_PASSWORD
+    else:
+        return massage.ERROR_USER_NOT_EXIST
