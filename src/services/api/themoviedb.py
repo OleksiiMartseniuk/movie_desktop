d8f9565354e58b8dda5de39e99af0693da31703e
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-12-04 21:45:47 +0200

    create class TheMovieDatabaseApi

---
+++ src/services/api/themoviedb.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class TheMovieDatabaseApi:
+
+    def __init__(
+        self,
+        api_key: str,
+        url: str = "https://api.themoviedb.org/",
+        version: int = 3,
+        language: str = 'ru-Ru',
+    ) -> None:
+        self.url = f'{url}{version}'
+        self.api_key = api_key
+        self.language = language
