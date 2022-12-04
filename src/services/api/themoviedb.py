1f30815a6216579e9bf1fc9876451865791d91d2
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-12-04 22:45:39 +0200

    Get the primary movie/TV show details by id.

--- src/services/api/themoviedb.py
+++ src/services/api/themoviedb.py
@@ -1,6 +1,8 @@
 import requests
 import logging
 
+from typing import Optional
+
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +24,7 @@ class TheMovieDatabaseApi:
         try:
             response = requests.get(url=url, **kwargs)
             if response.status_code == 200:
-                response.json()
+                return response.json()
             else:
                 logger.error(
                     f'status code [{response.status_code}] url [{url}]'
@@ -42,3 +44,21 @@ class TheMovieDatabaseApi:
             params_add = {key: value for key, value in kwargs.items() if value}
             params.update(params_add)
         return params
+
+    def get_details(self, id: int, type: str) -> Optional[dict]:
+        """Получить первичную информацию movie/TV
+
+        Parameters
+        ----------
+        id : int
+            ID movie/TV
+        type : str
+            ['movie', 'tv']
+        """
+        if type != 'movie' and type != 'tv':
+            logger.error('Invalid parameter [type]')
+            return None
+
+        url = f'{self.url}/{type}/{id}'
+        params = self.__set_params()
+        return self.get(url=url, params=params)
