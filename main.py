dced5ae79fb43d1ea43a405b83f81c008f1a88f1
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-12-25 15:45:19 +0200

    add ui Form

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from src.ui.auth import Auth
+from src.ui.form import Form
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    auth = Auth()
+    form = Form()
+    auth = Auth(ui_form=form)
     auth.show()
 
     sys.exit(app.exec())
