097941d87da2407fdd48049b6233c4b847169b46
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-12-31 16:00:06 +0200

    adding function form_update_valid

--- src/utils/validation.py
+++ src/utils/validation.py
@@ -1,6 +1,8 @@
 import re
 
-from typing import Optional, List, Union
+from typing import Optional, List, Tuple, Union
+
+from src.services.auth.security import verify_password
 
 from . import massage
 
@@ -61,5 +63,52 @@ def form_valid(
         )
         if confirm_password_error:
             error_message_list.append(confirm_password_error)
-
     return error_message_list
+
+
+def form_update_valid(
+    new_username: str,
+    new_password: str,
+    confirm_password: str,
+    hash_password: str,
+    current_username: str
+) -> Tuple[list, list]:
+    """Валидация формы обновления профиля пользователя
+    --------------------------------------
+    >>> 'action' Активные поля для изменений
+    >>> 'error_message_list' Список ошибок
+    """
+    error_message_list = []
+    # поля для изменений ['username', 'password']
+    action = []
+
+    if new_username:
+        action.append('username')
+        username_error = username_valid(new_username)
+        if username_error:
+            error_message_list.append(username_error)
+        if new_username == current_username:
+            error_message_list.append(massage.ERROR_NEW_USERNAME)
+
+    if ((new_password and not confirm_password)
+            or (not new_password and confirm_password)):
+        error_message_list.append(massage.ERROR_PASSWORD_AND_CONFIRM)
+
+    if new_password and confirm_password:
+        action.append('password')
+        password_error = password_valid(new_password)
+        if password_error:
+            error_message_list.append(password_error)
+
+        confirm_password_error = password_confirm_valid(
+            new_password, confirm_password
+        )
+        if confirm_password_error:
+            error_message_list.append(confirm_password_error)
+        if verify_password(new_password, hash_password):
+            error_message_list.append(massage.ERROR_NEW_PASSWORD)
+
+    if len(action) < 1:
+        error_message_list.append(massage.ERROR_ACTION)
+
+    return action, error_message_list
