6b95d41c3de2ac783812fa29ac91328dfa5c93a5
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-12-31 12:04:28 +0200

    add settings LOGGING_CONFIG

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,3 +10,38 @@ ICON_DIR = BASE_DIR.joinpath('icon')
 MONGO_DB_URL = os.getenv('MONGO_DB_URL')
 
 KEY_THEMOVIEDB = os.getenv('KEY_THEMOVIEDB')
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "standard": {
+            "format": ("%(asctime)s\t%(name)s:%(module)s[%"
+                       "(lineno)d]:%(levelname)s %(message)s")
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+            "level": "INFO",
+            "stream": "ext://sys.stdout"
+        },
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "standard",
+            "level": "INFO",
+            "filename": "info.log",
+            "mode": "a",
+        }
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console"],
+            "level": "INFO"
+        },
+        "full": {
+            "handlers": ["console", "file"],
+            "level": "INFO"
+        }
+    }
+}
