dd73a8f876161fe844b9b7899772d8689beb0771
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-01-15 14:16:01 +0200

    create cart

--- src/utils/ui.py
+++ src/utils/ui.py
@@ -1,4 +1,8 @@
-from PySide6.QtWidgets import QVBoxLayout, QLabel
+import requests
+
+from PySide6.QtWidgets import QVBoxLayout, QLabel, QWidget, QPushButton
+from PySide6.QtGui import QFont, QPixmap, QImage
+from PySide6.QtCore import QSize, Qt
 
 
 def create_message(text: str, color: str) -> QLabel:
@@ -19,3 +23,42 @@ def clear_massage(layout: QVBoxLayout):
     """Очистка сообщений с формы"""
     for i in reversed(range(layout.count())):
         layout.itemAt(i).widget().setParent(None)
+
+
+def cart(id: int, title: str, image_url: str, data: str) -> QWidget:
+    """Cart"""
+    cart = QWidget()
+    layout = QVBoxLayout(cart)
+
+    cart_image = QLabel(cart)
+    cart_image.setMinimumSize(QSize(150, 225))
+    cart_image.setMaximumSize(QSize(150, 225))
+    cart_image.setStyleSheet(u"border-radius: 15px;""")
+
+    image = QImage()
+    image.loadFromData(requests.get(image_url).content)
+    cart_image.setPixmap(QPixmap(image))
+
+    layout.addWidget(cart_image)
+
+    font2 = QFont()
+    font2.setPointSize(12)
+    font2.setBold(True)
+    cart_title = QPushButton(cart)
+    cart_title.setText(title)
+    cart_title.setMaximumSize(QSize(150, 16777215))
+    cart_title.setFont(font2)
+
+    layout.addWidget(cart_title)
+
+    cart_date = QLabel(cart)
+    cart_date.setText(data)
+    cart_date.setMaximumSize(QSize(150, 16777215))
+    font = QFont()
+    font.setPointSize(10)
+    font.setBold(False)
+    cart_date.setFont(font)
+    cart_date.setAlignment(Qt.AlignCenter)
+
+    layout.addWidget(cart_date)
+    return cart
